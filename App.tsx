c96d436e626bedcd362f4ea75f19a1b86d4663b2
Mauricio Martinez <maumarcastil@gmail.com>
2023-08-19 15:07:37 -0500

    feat: include RooStack in the Root application

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import RootStack from './src/stacks/RootStack';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RootStack />;
 }
 
 const styles = StyleSheet.create({
