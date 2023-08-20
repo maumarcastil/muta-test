6716383cfd034cffad7192351d2b5e1cc0bc5778
Mauricio Martinez <maumarcastil@gmail.com>
2023-08-19 20:36:22 -0500

    feat: new utilities to convert numbers into currencies

---
+++ src/utils/convert.tsx
@@ -0,0 +1,8 @@
+export const numberFormat = (value: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(value);
+};
